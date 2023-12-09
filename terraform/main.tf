2d37a36f7746cb6ee1728fbe16e4da5bc9b5071d
KD Puvvadi <rao@puvvadi.me>
2023-12-09 14:49:18 +0530

    terraform config for cloudflare pages

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,67 @@ data "cloudflare_zones" "zone_puvvadi_me" {
     name = "puvvadi.me"
   }
 }
+
+# Cloudflare project deployment and DNS record for the project
+
+resource "cloudflare_pages_project" "omadac_pages_project" {
+  account_id        = data.cloudflare_accounts.cloudflare_account_data.accounts[0].id
+  name              = "omadac"
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner                         = "kdpuvvadi"
+      repo_name                     = "omadac"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "all"
+      preview_branch_includes       = ["*"]
+      preview_branch_excludes       = ["main", "prod"]
+    }
+  }
+
+  build_config {
+    build_command       = "yarn build"
+    destination_dir     = "build"
+    root_dir            = ""
+    web_analytics_tag   = "3505589a6f1e46079b66ba27cb62ca4a"
+    web_analytics_token = "99506fb0992b45d4a482d341c442ec6e"
+  }
+
+  deployment_configs {
+    preview {
+      environment_variables = {
+        NODE_VERSION = "18.19.0"
+        YARN_VERSION  = "1.22.19"
+      }
+      fail_open = true
+    }
+    production {
+      environment_variables = {
+        NODE_VERSION = "18.19.0"
+        YARN_VERSION  = "1.22.19"
+      }
+      fail_open = true
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "omadac_domain" {
+  account_id   = data.cloudflare_accounts.cloudflare_account_data.accounts[0].id
+  project_name = cloudflare_pages_project.omadac_pages_project.name
+  domain       = "omadac.puvvadi.me"
+}
+
+resource "cloudflare_record" "cloudflare_omada_record" {
+  zone_id         = data.cloudflare_zones.zone_puvvadi_me.zones[0].id
+  name            = "omadac"
+  value           = cloudflare_pages_project.omadac_pages_project.subdomain
+  type            = "CNAME"
+  proxied         = true
+  ttl             = 1
+  allow_overwrite = true
+}
